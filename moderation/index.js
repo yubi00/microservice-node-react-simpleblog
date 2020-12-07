19ccb402f4f24a7f481a18de67f24b8d0f4fb0ee
Yuba Raj Khadka <ubrajkhadka@gmail.com>
2020-12-07 23:04:35 +1100

    kubernetes services setup

--- moderation/index.js
+++ moderation/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const handleEvent = async (type, data) => {
   if (type === 'CommentCreated') {
     const status = data.content.includes('orange') ? 'rejected' : 'approved';
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentModerated',
       data: {
         id: data.id,
@@ -28,7 +28,7 @@ app.post('/events', async (req, res) => {
 
 app.listen(4003, async () => {
   console.log('Moderation service started at 4003');
-  const res = await axios.get('http://localhost:4005/events');
+  const res = await axios.get('http://event-bus-srv:4005/events');
   for (let event of res.data) {
     console.log('Processing event: ', event.type);
     handleEvent(event.type, event.data);
--- posts/index.js
+++ posts/index.js
@@ -22,7 +22,7 @@ app.post('/posts', async (req, res) => {
     title
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id,
@@ -39,5 +39,5 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('Post service started on 4000');
+  console.log('Post service started on port 4000');
 });
